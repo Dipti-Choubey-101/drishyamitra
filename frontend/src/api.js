de26662fddbd49b94a8b69c2859dc50e8bebf530
Diptanil-Sen <diptanilsen10@gmail.com>
2026-03-05 01:05:24 +0530

    update API URL for production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-baseURL: 'https://drishyamitra.onrender.com/api',});
+  baseURL: process.env.REACT_APP_API_URL || 'http://127.0.0.1:5000/api',
+});
 
 // Automatically attach token to every request
 API.interceptors.request.use((config) => {
